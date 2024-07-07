590bedeef953bbc6a2be5e763e1ce425da636e83
aloncrack7 <alonmadrid@gmail.com>
2024-07-07 13:01:16 +0200

    Add: Database for mlflow

--- install.sh
+++ install.sh
@@ -39,10 +39,11 @@ fi
 
 read -p "Do you want to make changes in the .env file? (y/N): " change_env
 
-if [ ! -z $change_env ] && ([ $store_config = 'y' ] || [ $store_config = 'Y' ]); then
+if [[ ! -z $change_env ]] && ([[ $change_env = 'y' ]] || [[ $change_env = 'Y' ]]); then
     read -p "Do you want to store the configuration in .env file? (y/N): " store_config
     set_up_enviroment_variable "MLFLOW_DOMAIN" "mlflow domain" $store_config
     set_up_enviroment_variable "STATUS_DOMAIN" "status doamin" $store_config
+    set_up_enviroment_variable "POSTGRES_PASSWORD" "postgres password" $store_config
 fi
 
 . .env
